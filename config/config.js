7a09d9531703da3f2c26f5a3c8e19e8a0b73e1c4
marandaz <mrzwies@gmail.com>
2021-01-08 22:47:37 -0600

    dotenv

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+  "development": {
+    "username": "lr5klkeebsblvltq",
+    "password": "y9bhva0h2v6lpd1t",
+    "database": "hqw2ijwt4aa7lg75",
+    "host": "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "lr5klkeebsblvltq",
+    "password": "y9bhva0h2v6lpd1t",
+    "database": "hqw2ijwt4aa7lg75",
+    "host": "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "lr5klkeebsblvltq",
+    "password": "y9bhva0h2v6lpd1t",
+    "database": "hqw2ijwt4aa7lg75",
+    "host": "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+}
